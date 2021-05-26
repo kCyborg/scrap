18e37650c2d1f4d9ae2e95df288a26e475d81e10
kCyborg <kcyborg2.1992@gmail.com>
2021-05-26 00:08:25 -0400

    I added a little interactive way of ask the user for info in order to build the epub

--- scrap.py
+++ scrap.py
@@ -2,7 +2,17 @@ from bs4 import BeautifulSoup as bs
 import requests
 from ebooklib import epub
 
-next_url = "https://wanderinginn.com/2021/01/10/8-00/"
+print ("Hi, this little piece of code will format The Wondering Inn web serial \
+into an epub")
+
+next_url = input("Enter the first chapter of the volumen url: ")
+n_chapters = int(input("Enter the numbers of chapters you wanna scrap: "))
+n_chapters = n_chapters - 1
+description = input("Enter a little description: ")
+book_name_epub = input("Enter the book's name (inside the epub): ")
+book_name_file = input("Enter the book's name (file name): ")
+
+#next_url = "https://wanderinginn.com/2021/01/10/8-00/"
 
 chapter_list = []
 
@@ -13,21 +23,18 @@ def scraping(url):
     soup = bs(html, "lxml")
 
     #Get the title
-    # global title
     title = soup.title.get_text()
     title = title.split('|')[0]
     title = title[:-1]
     #print(title)
 
     #Get the chapter
-    # global chapter
     chapter = soup.find("div", class_="entry-content")
     chapter = str(chapter)
     #print(chapter)
 
     span = soup.find('span', class_="nav-next")
     a = span.find('a', href=True)
-    # global next_url
     next_url = a['href']
 
     return (title, chapter, next_url)
@@ -37,17 +44,17 @@ def scraping(url):
 book = epub.EpubBook()
 
 book.set_identifier('id131313')
-book.set_title('The Wondering Inn Volumen 8 (so far)')
+book.set_title(book_name_epub)
 book.set_language('en')
 
 book.add_author('PirateAba')
 
 c1 = epub.EpubHtml(title='Introduction', file_name="intro.xhtml", lang='en')
-c1.content = 'An inn is a place to rest, a place to talk and share stories, or a place to find adventures, a starting ground for quests and legends. In this world, at least. To Erin Solstice, an inn seems like a medieval relic from the past. But here she is, running from Goblins and trying to survive in a world full of monsters and magic. She’d be more excited about all of this if everything wasn’t trying to kill her. But an inn is what she found, and so that’s what she becomes. An innkeeper who serves drinks to heroes and monsters– Actually, mostly monsters. But it’s a living, right? This is the story of the Wandering Inn.'
+c1.content = description
 book.add_item(c1)
 
 i=0
-while i<=5:
+while i<=n_chapters:
     (title, chapter, next_url) = scraping(next_url)
 
     fle_name = "C" + str(i + 2) + ".xhtml"
@@ -88,7 +95,7 @@ book.add_item(nav_css)
 book.spine = ['nav', c1] + chapter_list
 
 # write to the file
-epub.write_epub('The Wondering Inn Vol8 (so far).epub', book, {})
+epub.write_epub(book_name_file+'.epub', book, {})
 
 
 
